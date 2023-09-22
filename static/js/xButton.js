93ec5f98629187b1fe3d660de66171896cdb2e70
Audun Johansen <audunjohansen@live.no>
2023-09-22 22:00:21 +0200

    make x button disappear after pressing it

--- static/js/xButton.js
+++ static/js/xButton.js
@@ -1,16 +1,32 @@
 const textarea = document.querySelector('textarea[name="user_input"]');
-const xButton = document.getElementById('clear_text');
+const xButton = document.querySelector('.x-button');
+const xButtonImg = xButton.querySelector('img');
 
 textarea.addEventListener('input', () => {
     if (textarea.value) {
-        xButton.style.display = 'block';
+        xButtonImg.style.display = 'block';
+        xButton.setAttribute('tabindex', '0'); // Make the 'x' button focusable
     } else {
-        xButton.style.display = 'none';
+        xButtonImg.style.display = 'none';
+        xButton.removeAttribute('tabindex'); // Make the 'x' button not focusable
     }
 });
 
-xButton.addEventListener('click', () => {
-    event.preventDefault();
+function clearTextarea() {
     textarea.value = '';
     textarea.focus();
+    // Manually trigger the input event
+    var inputEvent = new Event('input', {
+        bubbles: true,
+        cancelable: true,
+    });
+    textarea.dispatchEvent(inputEvent);
+}
+
+xButton.addEventListener('click', clearTextarea);
+xButton.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        clearTextarea();
+    }
 });
